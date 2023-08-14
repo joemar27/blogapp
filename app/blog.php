f8eaf17ec70e2397af4f7e67adec267b4132b16f
Joseph Hernandez <josephmar.hernandez@gmail.com>
2023-08-14 13:03:44 -0500

    Adds Content and updated pages
    Adding about content to about page.  Added header and footers to about
    and archive page.  Added placeholder to archive page.

--- app/blog.php
+++ app/blog.php
@@ -1,3 +1,8 @@
 <?php
-    echo "blog";
+    require 'header.php';
+    require '_nav_bar.php';
+    echo "<div class='container-fluid text-center'><h1>Coming Soon</h1></div>";
+?>
+<?php
+    require 'footer.php';
 ?>
